54b881b531cff7dab682c5e1b8f3f3305719bd55
binbin.cai <binbin.cai@alphainsight.ai>
2019-06-26 16:38:09 +0800

    NLP theory and practice task 3

---
+++ task3/sklearn_TfidfVectorizer_example.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# sklearn提取文本tfidf特征
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+corpus = [
+    'this is the first document',
+    'this is the second second document',
+    'and the third one',
+    'is this the first document'
+]
+
+tfidf_vec = TfidfVectorizer()
+tfidf_matrix = tfidf_vec.fit_transform(corpus)
+
+# 得到语料库所有不重复的词
+print(tfidf_vec.get_feature_names())
+
+# 得到每个单词对应的id值
+print(tfidf_vec.vocabulary_)
+
+# 得到每个句子所对应的向量
+# 向量里数字的顺序是按照词语的id顺序来的
+print(tfidf_matrix.toarray())
